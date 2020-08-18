06f8801780dfbd95531a2bbd91ccc35587fdfff6
Leon Carnagie <leonfross@gmail.com>
2020-08-18 12:10:59 -0700

    Create component files

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header(){
+return(
+    <header>
+        <nav className="navbar navbar-light bg-light">
+  <span className="navbar-text">
+  Leon's Bar-HR-System
+  </span>
+</nav>
+    </header>
+)
+}
+export default Header;
